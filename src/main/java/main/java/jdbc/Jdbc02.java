607522f18e94568c47c32a3aad04b6ffd0758330
khj0105 <khj0001052@gmail.com>
2025-04-22 12:48:35 +0900

    20250422 JDBC/JAVA

---
+++ src/main/java/main/java/jdbc/Jdbc02.java
@@ -0,0 +1,31 @@
+package main.java.jdbc;
+
+/*
+* ==== JDBC 환경설정 ====
+*
+* 1. 필요한 준비물
+*   1) JDK(JAVA - JDK 17)
+*   2) IDE(IntelliJ, Eclipse 등)
+*   3) MySQL (MySQL Server 8.x 이상 권장)
+*   4) JDBC 드라이버
+*       : mysql-connector-java-x.x.xx.jar
+* 
+*   +) SQL 스키마, Java 프로젝트
+*
+* */
+
+//=== 자주 발생하는 오류 & 해결법 ===//
+// 1. ClassNotFoundException
+// : 외부 라이브러리(sql connector)가 프로젝트에 추가되지 않음)
+
+// 2. Access denied for user
+// : DB 사용자 ID/PW 오타 또는 권한 없음
+
+// 3. Communications link failure
+// : MySQL 서버가 실행 중인지 확인 (포트번호 확인 3306)
+
+// 4. Unknown database
+// : DB 이름 오타 또는 미생성
+
+public class Jdbc02 {
+}
